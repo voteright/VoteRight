f054a50f449b8145e80bf875627813eb373743e9
Joseph Lyon <joeylyon1@gmail.com>
2019-04-15 10:19:27 -0400

    update test files

--- election/election_test.go
+++ election/election_test.go
@@ -282,3 +282,103 @@ func TestElection_CheckVerificationCountsMatch(t *testing.T) {
 		t.Errorf("Vote totals do not match but they should")
 	}
 }
+
+func TestElection_GetAllRaces(t *testing.T) {
+	testDatabase := database.StormDB{
+		File: "election_testdb.db",
+	}
+	err := testDatabase.Connect()
+	if err != nil {
+		t.Errorf("Couldn't connect to database. Error: %s", err.Error())
+	}
+
+	testCandidate := models.Candidate{
+		ID:     1,
+		Cohort: 1,
+		Name:   "Joey Lyon",
+	}
+	testCandidateTwo := models.Candidate{
+		ID:     2,
+		Cohort: 1,
+		Name:   "Joey Lyon Two",
+	}
+
+	err = testDatabase.StoreCandidate(testCandidate)
+	if err != nil {
+		t.Errorf("Couldn't add test candidate to database")
+	}
+	err = testDatabase.StoreCandidate(testCandidateTwo)
+	if err != nil {
+		t.Errorf("Couldn't add test candidate to database")
+	}
+	race := models.Race{
+		ID:         1,
+		Name:       "Test",
+		Candidates: []int{1, 2},
+	}
+
+	e := New(&testDatabase, false, []string{})
+	resp, err := e.GetAllRaces()
+	if err != nil {
+		t.Errorf("Couldn't add test candidate to database")
+	}
+	if len(resp) != 0 {
+		t.Errorf("Got races when there were none in the database")
+	}
+	e.db.StoreRace(race)
+
+	err = os.Remove("election_testdb.db")
+	if err != nil {
+		t.Log("Cleanup failed")
+	}
+}
+
+func TestElection_GetVoterByID(t *testing.T) {
+	testDatabase := database.StormDB{
+		File: "election_testdb.db",
+	}
+	err := testDatabase.Connect()
+	if err != nil {
+		t.Errorf("Couldn't connect to database. Error: %s", err.Error())
+	}
+	testVoter := models.Voter{
+		StudentID: 1,
+		Cohort:    1,
+		Name:      "Prof Sturman",
+	}
+	testVoterWontVote := models.Voter{
+		StudentID: 2,
+		Cohort:    1,
+		Name:      "Prof Goldschmidt",
+	}
+
+	err = testDatabase.StoreVoter(testVoter)
+	if err != nil {
+		t.Errorf("Couldn't add test voter to database")
+	}
+	err = testDatabase.StoreVoter(testVoterWontVote)
+	if err != nil {
+		t.Errorf("Couldn't add test voter to database")
+	}
+
+	e := New(&testDatabase, false, []string{})
+
+	voter, err := e.GetVoterByID(1)
+	if err != nil {
+		t.Errorf("Unexpected error in grabbing voter")
+	}
+	if *voter != testVoter {
+		t.Errorf("Failed to retreive proper voter")
+	}
+	voter, err = e.GetVoterByID(400)
+	if err != nil {
+		t.Errorf("unexpected error grabbing voter")
+	}
+	if voter != nil {
+		t.Errorf("voter was returned for invalid id")
+	}
+	err = os.Remove("election_testdb.db")
+	if err != nil {
+		t.Log("Cleanup failed")
+	}
+}
--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
-go test ./database/ -coverprofile > coverageReport.txt
-go test ./config/ -coverprofile >> coverageReport.txt
-go test ./cmd/ -coverprofile >> coverageReport.txt  
-go test ./election/ -coverprofile >> coverageReport.txt
-go test ./models/ -coverprofile >> coverageReport.txt
-go test ./api/ -coverprofile >> coverageReport.txt
+go test ./database/ -coverprofile fmt > coverageReport.txt
+go test ./config/ -coverprofile fmt >> coverageReport.txt
+go test ./cmd/ -coverprofile fmt >> coverageReport.txt  
+go test ./election/ -coverprofile fmt >> coverageReport.txt
+go test ./models/ -coverprofile fmt >> coverageReport.txt
+go test ./api/ -coverprofile fmt >> coverageReport.txt
 cat coverageReport.txt
 # go test ./verificationapi/
